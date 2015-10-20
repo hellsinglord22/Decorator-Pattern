7ac500bb232206c2a141304134049d4227cb1e04
H <heshamscorpe55@gmail.com>
2015-10-20 17:14:33 +0200

    Beverage Superclass
    used to make both Conc.Beverage and Decorator the same Super type

---
+++ src/Beverage.java
@@ -0,0 +1,16 @@
+
+public abstract class Beverage {
+
+	/// Attributes //
+	protected String description = "unknow beverage";
+
+	// Methods //
+	public abstract double coast();
+
+	public String getDescription(){
+		return description;
+	}
+
+
+
+}
